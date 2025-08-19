8f3d1d9a2f71a4300ce2974db68a0b99e4a129dd
thewtheow <56664924+thewtheow@users.noreply.github.com>
2025-08-19 16:38:23 +0700

    Add files via upload

---
+++ letter.js
@@ -0,0 +1,38 @@
+let heart = document.querySelector(".heart");
+let card = document.querySelector(".card");
+let box = document.querySelector("#box");
+heart.addEventListener("click", function () {
+  card.setAttribute("style", "opacity:0");
+  let x = document.createElement("audio");
+  x.setAttribute("src", "qlx.mp3");
+  x.setAttribute("autoplay", "autoplay");
+
+  let i = 0;
+  let str =
+    "Hi tinkerbell,<Kalo lo udah bisa baca tulisan ini, Tandanya lo sudah menerima seekor stitch betina yang berwarna pink ，Terimakasih telah menerima barang tersebut ya^^, Semoga dengan kehadiran barang tersebut lo suka, bisa nemenin lo dan di jauhkan dari segala macam overthinking. Stay safe, Stay healthy<and Stay with me xixixixixi,<Allahumma lakasumtu, i can't stop thinking about you,<Wabika amantu, my hear is on for you,<Wa'ala rizkika aftortu, in my heart of course only you.<mie goreng mie kuah, muah-muah<See u next year<-Random Person-";
+  let strp = "";
+
+  function print() {
+    if (str[i] == "<") {
+      document.getElementById("box").innerHTML = strp + "<br><br>+'|'";
+      strp += "<br><br>";
+    } else {
+      strp += str[i];
+      box.innerHTML = strp + "|";
+    }
+    i++;
+  }
+  setTimeout(() => {
+    let printid = setInterval(() => {
+      print();
+      if (i == str.length) clearInterval(printid);
+    }, 190);
+  }, 5500);
+
+  function appearBackground() {
+    setTimeout(() => {
+      box.style.opacity = 1;
+    }, 1500);
+  }
+  appearBackground();
+});
